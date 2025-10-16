c48e7d3bd620bc312edb89a0d06d90639b6be325
xpri <jaspil007@gmail.com>
2025-10-16 18:29:37 -0400

    Changed discord bot token to testbench and edited some of the main.cpp code.

--- DiscordBotCpp/src/BOT_TOKEN.cpp
+++ DiscordBotCpp/src/BOT_TOKEN.cpp
@@ -1,3 +1,3 @@
 #include <iostream>
 
-const std::string BOT_TOKEN = "MTA5MDk0NzA0NDU4NTI0MjcxNA.GqqF_j.-lhG1OnKzUHOtutZaUrT8NqESCYwhLfN0wBtcg";
+const std::string BOT_TOKEN = "MTMwMzUxNzc1MDkyMDA4NTU5NA.GAzaLt.rFPI-Ye_u1pzhZYegXUod2uywZ_A6Yf_D6EZOI";
--- DiscordBotCpp/src/main.cpp
+++ DiscordBotCpp/src/main.cpp
@@ -4,54 +4,65 @@
 #include <iostream>
 #include "BOT_TOKEN.cpp"
 
-/* Be sure to place your token in the line below.
- * Follow steps here to get a token:
- * https://dpp.dev/creating-a-bot-application.html
- * When you invite the bot, be sure to invite it with the
- * scopes 'bot' and 'applications.commands', e.g.
- * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
- */
-
 int main()
 {
 	/* Create bot cluster */
-	dpp::cluster bot(BOT_TOKEN);
+	//dpp::cluster bot(BOT_TOKEN);
+
+	dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
 
 	/* Output simple log messages to stdout */
 	bot.on_log(dpp::utility::cout_logger());
 
 	/* Register slash command here in on_ready */
-	bot.on_ready([&bot](const dpp::ready_t& event) {
+	bot.on_ready([&bot](const dpp::ready_t& event)
+	{
 		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
-		if (dpp::run_once<struct register_bot_commands>()) {
-			std::vector<dpp::slashcommand> commands{
+		if (dpp::run_once<struct register_bot_commands>())
+		{
+			std::vector<dpp::slashcommand> commands
+			{
 				{ "ping", "Ping pong!", bot.me.id }
 			};
 
 			bot.global_bulk_command_create(commands);
 		}
-		});
+	});
 
-	/* Handle slash command with the most recent addition to D++ features, coroutines! */
-	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
-		if (event.command.get_command_name() == "ping") {
+	// The event is fired when someone issues your commands
+	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void>
+	{
+		if (event.command.get_command_name() == "ping")
+		{
 			co_await event.co_reply("Pong!");
 		}
 		co_return;
-		});
+	});
+
+
 
 	/* The event is fired when the bot detects a message in any server and any channel it has access to. */
-	bot.on_message_create([&bot](const dpp::message_create_t& event) {
-		/* See if the message contains the phrase we want to check for.
-		 * If there's at least a single match, we reply with a message.
-		 */
-		if (event.msg.content.find("check this") != std::string::npos) {
-			event.reply("Nice try, but I know you said \"check this\"! ", true);
+	bot.on_message_create([&bot](const dpp::message_create_t& event)
+	{
+		// Checks if the author of the message is from the bot itself.
+		if (event.msg.author.is_bot())
+		{
+			return;
 		}
-		});
+		// See if the message contains the phrase we want to check for.
+		// If there's at least a single match, we reply with a message.
+		// DOES NOT CHECK IF IT IS FROM THE BOT ITSELF.
+		
+		std::string ChkTs = "what the ink";
+
+		if (event.msg.content.find(ChkTs) != std::string::npos)
+		{
+			event.reply("Nice try, but I know you said \"" + ChkTs + "\!""", true);
+		}
+	});
 
 	/* Start the bot */
 	bot.start(dpp::st_wait);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
