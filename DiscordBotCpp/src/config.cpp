00dec81a1bb47788a8b794360ba382fca21d2a28
xpri <jaspil007@gmail.com>
2025-10-22 14:06:40 -0400

    Making some adjustments so I can make an API call to gemini.

---
+++ DiscordBotCpp/src/config.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+const std::string BOT_TOKEN = "MTMwMzUxNzc1MDkyMDA4NTU5NA.GAzaLt.rFPI-Ye_u1pzhZYegXUod2uywZ_A6Yf_D6EZOI";
+const std::string GEMINI_API_KEY = "your_actual_key_here";
